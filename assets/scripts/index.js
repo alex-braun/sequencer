c240e8e727e2d12436927f409fa653cef9bb520d
alex-braun <braunacb@gmail.com>
2016-11-09 21:43:29 -0500

    Created Readme.
    Created readme explaining the basic design of the audio context.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,6 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 require('./sequencer');
-require('./kit');
 const sequencer = require('./sequencer');
 
 $(() => {
